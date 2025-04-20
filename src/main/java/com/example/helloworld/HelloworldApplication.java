938fb937938942624ad8ce076b7676fd63c562ce
ZBook <med.hmaoud@gmail.com>
2025-04-20 16:58:23 +0200

    init commit

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.RecursiveTask;
+
 @SpringBootApplication
 @RestController
+
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +19,9 @@ public class HelloworldApplication {
 @RequestMapping("/")
 	public String helloworld() {
 		return ("helloworld Hmaoud Mohamed, wie geht's dir");
-}
+
+	}
 
 }
+
+
